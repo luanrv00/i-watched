0d8440bd33ee9a7f727bf529975197e5b3c347d3
luan ramos vicente <luanrv00@gmail.com>
2024-09-02 10:21:21 -0300

    fixes Inder font usage

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inder } from "next/font/google";
 import "./globals.css";
 
-const inder = Inder({ subsets: ["latin"] });
+const inder = Inder({ subsets: ["latin"], weight: '400'});
 
 export const metadata: Metadata = {
   title: "Create Next App",
