0fc1f7325baf2890d0e297a8c871237dcd827da1
luan ramos vicente <luanrv00@gmail.com>
2024-09-08 17:18:33 -0300

    feat: styles searched list

--- src/app/(home)/ui/SearchMatches/index.tsx
+++ src/app/(home)/ui/SearchMatches/index.tsx
@@ -10,15 +10,26 @@ export function SearchMatches({
     return
   }
 
-  return matchesList?.map((matchItem: WatchItemType) => (
-    <div key={matchItem.tmdbId} className='flex flex-row'>
-      <Image src={matchItem.posterUrl} width='100' height='150' alt='cover' />
-      <div className='ml-2'>
-        <p>{matchItem.title}</p>
-        <p>{matchItem.releaseYear}</p>
-        {matchItem.seasonsCount && <p>seasons: {matchItem.seasonsCount}</p>}
-        {matchItem.episodesCount && <p>episodes: {matchItem.episodesCount}</p>}
-      </div>
+  return (
+    <div className='mt-5 flex flex-col gap-y-3'>
+      {matchesList?.map((matchItem: WatchItemType) => (
+        <div key={matchItem.tmdbId} className='flex flex-row'>
+          <Image
+            src={matchItem.posterUrl}
+            width='100'
+            height='150'
+            alt='cover'
+          />
+          <div className='ml-2'>
+            <p>{matchItem.title}</p>
+            <p>{matchItem.releaseYear}</p>
+            {matchItem.seasonsCount && <p>seasons: {matchItem.seasonsCount}</p>}
+            {matchItem.episodesCount && (
+              <p>episodes: {matchItem.episodesCount}</p>
+            )}
+          </div>
+        </div>
+      ))}
     </div>
-  ))
+  )
 }
