944ae14e9f53230d504ea975ccfe269b4ba05423
luan ramos vicente <luanrv00@gmail.com>
2024-09-11 20:26:34 -0300

    test: fix test SearchForm

--- e2e/search-form.spec.ts
+++ e2e/search-form.spec.ts
@@ -11,23 +11,48 @@ test.describe('search form', () => {
 
   test.describe('when searching', () => {
     test('shows items that match search', async ({page}) => {
-      await page.route('*/**/api/search', async route => {
-        const data = {
-          tmdb_id: 0,
-          poster_path:
-            'https://image.tmdb.org/t/p/w300_and_h450_bestv2/5KGQEaE519pOD9DltmWBo6OcuH1.jpg',
-          release_date: '2016-12-31',
-          title: 'title',
-          media_type: 'movie',
+      await page.route('*/**/api/search*', async route => {
+        const json = {
+          data: [
+            {
+              tmdbId: 0,
+              posterUrl:
+                'https://image.tmdb.org/t/p/w300_and_h450_bestv2/5KGQEaE519pOD9DltmWBo6OcuH1.jpg',
+              releaseYear: '2016',
+              title: 'title',
+              mediaType: 'movie',
+            },
+          ],
         }
-        await route.fulfill({json: data})
+        await route.fulfill({json})
       })
 
       await page.getByPlaceholder(/type anything/i).fill('matrix')
-      await expect(page.getByRole('listitem')).toHaveCount(4)
+      await expect(page.getByRole('listitem')).toHaveCount(1)
     })
 
-    test('matched items have an "add" button', ({page}) => {})
+    test('matched items have an "add" button', async ({page}) => {
+      await page.route('*/**/api/search*', async route => {
+        const json = {
+          data: [
+            {
+              tmdbId: 0,
+              posterUrl:
+                'https://image.tmdb.org/t/p/w300_and_h450_bestv2/5KGQEaE519pOD9DltmWBo6OcuH1.jpg',
+              releaseYear: '2016',
+              title: 'title',
+              mediaType: 'movie',
+            },
+          ],
+        }
+        await route.fulfill({json})
+      })
+
+      await page.getByPlaceholder(/type anything/i).fill('matrix')
+      const listItem = page.getByRole('listitem')
+      await expect(listItem.getByRole('button', {name: /add/i})).toBeVisible()
+    })
+
     test('clicking on "add" button adds the item to added list', ({page}) => {})
   })
 })
