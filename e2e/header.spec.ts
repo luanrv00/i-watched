ad767433a54e0ac023ea43650d5aad2166cc31e3
luan ramos vicente <luanrv00@gmail.com>
2024-09-23 18:52:25 -0300

    chore: prettier

--- e2e/header.spec.ts
+++ e2e/header.spec.ts
@@ -6,10 +6,16 @@ test.describe('header', () => {
   })
 
   test('renders "I\'ve Watched App" heading', async ({page}) => {
-    await expect(page.getByRole('heading', {level: 1, name: /I've Watched App/})).toBeVisible()
+    await expect(
+      page.getByRole('heading', {level: 1, name: /I've Watched App/})
+    ).toBeVisible()
   })
 
-  test('renders "Track your Animes, TV Series and Movies progress!" description', async ({page}) => {
-    await expect(page.getByText(/Track your Animes, TV Series and Movies progress!/)).toBeVisible()
+  test('renders "Track your Animes, TV Series and Movies progress!" description', async ({
+    page,
+  }) => {
+    await expect(
+      page.getByText(/Track your Animes, TV Series and Movies progress!/)
+    ).toBeVisible()
   })
 })
