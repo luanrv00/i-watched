0e7d9b38e68fd92cd90cf470df7fd377c641a49e
luan ramos vicente <luanrv00@gmail.com>
2024-10-15 16:41:50 -0300

    test: SearchMatches when has not matches

--- src/app/(home)/ui/SearchMatches/index.test.tsx
+++ src/app/(home)/ui/SearchMatches/index.test.tsx
@@ -4,13 +4,25 @@ import {watchItemFixture} from '../../../../../fixtures'
 import type {WatchItemFullType} from '@/app/types/types'
 
 describe(SearchMatches, () => {
-  const matchesList: WatchItemFullType[] = [watchItemFixture]
+  describe('when has matches', () => {
+    const matchesList: WatchItemFullType[] = [watchItemFixture]
 
-  beforeEach(() => {
-    render(<SearchMatches matchesList={matchesList} />)
+    beforeEach(() => {
+      render(<SearchMatches matchesList={matchesList} />)
+    })
+
+    it('renders match items', () => {
+      expect(screen.getAllByRole('listitem')).toHaveLength(matchesList.length)
+    })
   })
 
-  it('renders match items', () => {
-    expect(screen.getAllByRole('listitem')).toHaveLength(matchesList.length)
+  describe('when has not matches', () => {
+    beforeEach(() => {
+      render(<SearchMatches matchesList={[]} />)
+    })
+
+    it('renders a message', () => {
+      expect(screen.getByText(/no results found/i)).toBeInTheDocument()
+    })
   })
 })
