d9008705bea64c78b625eb96b9eb06dce8a341f2
luan ramos vicente <luanrv00@gmail.com>
2024-10-15 16:37:52 -0300

    fix: escape Header character

--- src/app/ui/Header/index.tsx
+++ src/app/ui/Header/index.tsx
@@ -1,7 +1,7 @@
 export function Header() {
   return (
     <>
-      <h1 className='text-2xl'>I've Watched App</h1>
+      <h1 className='text-2xl'>I&apos;ve Watched App</h1>
       <p className='text-sm'>
         Track your Animes, TV Series and Movies progress!
       </p>
