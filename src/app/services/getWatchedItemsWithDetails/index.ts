03b235d7405b3fd822b8a767af863ee96967d0bb
luan ramos vicente <luanrv00@gmail.com>
2024-10-18 17:52:51 -0300

    feat: add service getWatchedItemsWithDetails

---
+++ src/app/services/getWatchedItemsWithDetails/index.ts
@@ -0,0 +1,29 @@
+import {TMDBItemType} from '@/app/types/types'
+import {getWatchedItems} from '../api'
+import {getMovieDetails} from '../getMovieDetails'
+import {getTvSeriesDetails} from '../getTvSeriesDetails'
+
+export async function getWatchedItemsWithDetails() {
+  const watchedItems = await getWatchedItems()
+
+  if (!watchedItems?.data.length) {
+    return {watchedTvSeries: null, watchedMovies: null}
+  }
+
+  const watchedTvSeries: TMDBItemType[] = []
+  const watchedMovies: TMDBItemType[] = []
+
+  for (const watchedItem of watchedItems.data) {
+    if (watchedItem.media_type === 'tv') {
+      const watchedItemDetails = await getTvSeriesDetails(watchedItem.id)
+      watchedTvSeries.push(watchedItemDetails)
+    }
+
+    if (watchedItem.media_type === 'movie') {
+      const watchedItemDetails = await getMovieDetails(watchedItem.id)
+      watchedMovies.push(watchedItemDetails)
+    }
+  }
+
+  return {watchedTvSeries, watchedMovies}
+}
