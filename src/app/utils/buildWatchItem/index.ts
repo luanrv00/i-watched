7ce64ee163c71ab817967ba9fb0a7de662c37e1c
luan ramos vicente <luanrv00@gmail.com>
2024-10-18 19:53:00 -0300

    fix: TMDBItemType title

--- src/app/utils/buildWatchItem/index.ts
+++ src/app/utils/buildWatchItem/index.ts
@@ -2,11 +2,13 @@ import {TMDB_POSTERS_URL} from '../../../app/constants'
 import type {TMDBItemType, WatchItemType} from '../../../app/types/types'
 
 export function buildWatchItem(tmdbData: TMDBItemType): WatchItemType {
+  const title = tmdbData?.title || (tmdbData?.name as string)
+
   return {
     tmdbId: tmdbData.id,
     posterUrl: `${TMDB_POSTERS_URL}/${tmdbData.poster_path}`,
     releaseYear: tmdbData.release_date?.split('-')[0],
-    title: tmdbData?.title || tmdbData?.name,
+    title,
     mediaType: tmdbData.media_type,
   }
 }
