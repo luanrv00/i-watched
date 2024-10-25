175307faf8d856001be8fb7fdcfeb427249028ba
luan ramos vicente <luanrv00@gmail.com>
2024-10-25 02:53:25 -0300

    wip: add WatchedItems

--- src/app/(home)/ui/WatchedItems/index.tsx
+++ src/app/(home)/ui/WatchedItems/index.tsx
@@ -1,20 +1,51 @@
 'use clitn'
-import {useEffect} from 'react'
+import {useEffect, useState} from 'react'
 import {getWatchedItemsWithDetails} from '@/app/services'
 import type { WatchItemType } from '@/app/types/types'
 
+export type WatchedItemsType = {
+  watchedMovies: WatchItemType[], 
+  watchedTvSeries: WatchItemType[]
+}
+
 export function WatchedItems() {
+  const [watchedItems, setWatchedItems] = useState<null|WatchedItemsType>(null)
+
   useEffect(() => {
     fetchWatchedItems()
   }, [])
 
   async function fetchWatchedItems() {
-    const watchedItems: {
-      watchedMovies: WatchItemType[], 
-      watchedTvSeries: WatchItemType[]
-    } = await getWatchedItemsWithDetails()
-    console.log('----------------watchedItems', watchedItems)
+    const watchedItems: WatchedItemsType = await getWatchedItemsWithDetails()
+    setWatchedItems(watchedItems)
+    //console.log('----------------watchedItems', watchedItems)
   }
 
-  return null
+  if(!watchedItems) {
+    return null
+    //return (
+    //  <p>Add watched TV Series</p>
+    //)
+  }
+
+  return (
+    <>
+      <div>
+        <h2>Watched TV Series</h2>
+        <ul>
+          {watchedItems.watchedTvSeries.map((watchedTVSeries: WatchItemType) => (
+            <li key={watchedTVSeries.tmdbId}>{watchedTVSeries.title}</li>
+          ))}
+        </ul>
+      </div>
+      <div>
+        <h2>Watched Movies</h2>
+        <ul>
+          {watchedItems.watchedMovies.map((watchedMovie: WatchItemType) => (
+            <li key={watchedMovie.tmdbId}>{watchedMovie.title}</li>
+          ))}
+        </ul>
+      </div>
+    </>
+  )
 }
