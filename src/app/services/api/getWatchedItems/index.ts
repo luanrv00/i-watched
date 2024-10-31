29a71477ea428b82dbd1a6d6703cdd3518efa180
luan ramos vicente <luanrv00@gmail.com>
2024-10-31 00:40:43 -0300

    fix: get watched items from local api

--- src/app/services/api/getWatchedItems/index.ts
+++ src/app/services/api/getWatchedItems/index.ts
@@ -1,11 +1,9 @@
 import type {WatchedItemDBType} from '@/app/types/types'
-import {TMDB_API_URL} from '../../../constants'
 
 export async function getWatchedItems(): Promise<{
   data: WatchedItemDBType[]
 }> {
-  const apiEndpoint = `${TMDB_API_URL}/api/shows/watched_items`
-  const watchedItems = await fetch(apiEndpoint)
+  const watchedItems = await fetch('/api/shows/watched_items')
     .then(res => res.json())
     .catch(e => console.log('------------------e', e))
     
